0e27ad5ca7c18f99904c47fd1af1d2b218393cfd
SamuelNady <medo.nadya82@gmail.com>
2023-11-18 18:18:21 +0200

    first commit

---
+++ assets/js/main.js
@@ -0,0 +1,10 @@
+const type = new Typed(
+    '#identity',
+    {
+        strings: ["Samuel", "A Frontend Web Developer"],
+        typeSpeed: 60,
+        backSpeed: 80,
+        startDelay: 1000,
+        loop: true,
+    }
+);
